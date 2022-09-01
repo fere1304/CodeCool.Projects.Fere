84d44ab9453921c71e53006c314f6dcfc0d1eebb
fere1304 <you@example.co>
2022-09-01 20:41:31 +0300

    Finished

--- helloreturn.js
+++ helloreturn.js
@@ -1,1 +1,10 @@
 // Write your JavaScript code here
+function getHelloMessage () {
+    let str = "Hello World!"
+    return str;
+}
+function sayHello() {
+    console.log (getHelloMessage());
+}
+sayHello();
+
