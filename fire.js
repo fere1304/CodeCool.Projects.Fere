fdbf2d05d92fbf9fca3de81e8e3542b86520e9fa
fere1304 <111707508+fere1304@users.noreply.github.com>
2022-10-22 02:10:43 +0300

    Delete fire.js

--- fire.js
+++
@@ -1,8 +0,0 @@
-mapCallbacks["fire"] = { code: 13, callback: potion1Callback, icon: "fa-solid fa-fire" };
-
-function potion1Callback(gameStats) {
-    console.log("You entered in the fire and died a terrible death :( ");
-    gameStats.playerHealth = 0;
-    gameStats.isGameRunning = 0;
-    return false;
-}
